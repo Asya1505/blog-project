a951e5dd1e115bedc2fd996850882c6b9cfa9c63
Асия Муксимова <asya@MacBook-Pro-Asia.local>
2023-05-19 12:02:02 +0300

    Add sidabar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,9 @@
-import { Link, Route, Routes } from "react-router-dom"
-import { Suspense } from 'react'
 import './styles/index.scss'
 import { classNames } from "shared/lib/classNames/classNames"
-import { AboutPage } from "pages/AboutPage"
-import { MainPage } from "pages/MainPage"
 import { useTheme } from "app/providers/ThemeProvider"
 import { Navbar } from "widgets/Nawbar"
+import { AppRouter } from "./providers/router"
+import { Sidebar } from "widgets/Sidebar"
 
 
 const App = () => {
@@ -14,12 +12,11 @@ const App = () => {
   return (
     <div className={classNames('app', {}, [theme])}>
       <Navbar />
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={'/about'} element={<AboutPage />} />
-          <Route path={'/'} element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      <div className='content-page'>
+        <Sidebar />
+        <AppRouter/>
+      </div>
+
     </div>
   )
 }
