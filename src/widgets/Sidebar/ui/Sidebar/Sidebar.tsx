40cd67ede90a7a9cb5b38c97486da5f4d01da77a
Асия Муксимова <asya@MacBook-Pro-Asia.local>
2023-05-22 13:30:33 +0300

    Add test react lib

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -17,8 +17,16 @@ export const Sidebar = ({ className }: SidebarProps) => {
   };
 
   return (
-    <div className={classNames(cls.sidebar, { [cls.collapsed]: collapsed }, [className])}>
-      <Button onClick={onToggle}>toggle</Button>
+    <div
+      data-testId="sidebar"
+      className={classNames(cls.sidebar, { [cls.collapsed]: collapsed }, [className])}
+    >
+      <Button
+        data-testId="sidebar-toggle"
+        onClick={onToggle}
+      >
+        toggle
+      </Button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
         <LangSwitcher className={cls.lang} />
