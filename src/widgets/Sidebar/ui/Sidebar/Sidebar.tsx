371f738772516831d616b0aad56192d67b80d55d
Асия Муксимова <asya@MacBook-Pro-Asia.local>
2023-05-23 15:26:01 +0300

    Test ci

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -4,18 +4,20 @@ import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { Button } from 'shared/ui/Button/Button';
 import cls from './Sidebar.module.scss';
 import { LangSwitcher } from 'shared/ui/LangSwitcher';
+import { useTranslation } from 'react-i18next';
 
 interface SidebarProps {
   className?: string
 }
 
 export const Sidebar = ({ className }: SidebarProps) => {
-  const [collapsed, setCollapsed] = useState(false);
+  const { t } = useTranslation()
+  const [collapsed, setCollapsed] = useState(false)
 
   const onToggle = () => {
-    setCollapsed((prev) => !prev);
-  };
-
+    setCollapsed((prev) => !prev)
+  }
+  
   return (
     <div
       data-testId="sidebar"
@@ -23,9 +25,9 @@ export const Sidebar = ({ className }: SidebarProps) => {
     >
       <Button
         data-testId="sidebar-toggle"
-        onClick={onToggle}
+        onClick={ onToggle }
       >
-        toggle
+        {t('Переключить')}
       </Button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
