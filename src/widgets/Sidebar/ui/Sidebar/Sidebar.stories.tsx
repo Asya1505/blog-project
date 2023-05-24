fca7b5ba5ec25ea3e38b35efe70b20ec634e663d
Асия Муксимова <asya@MacBook-Pro-Asia.local>
2023-05-24 21:05:36 +0300

    Fix tests

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
@@ -2,6 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
 import { Sidebar } from './Sidebar';
+import { BrowserRouter } from 'react-router-dom';
 
 const meta: Meta<typeof Sidebar> = {
   title: 'widget/Sidebar',
@@ -17,12 +18,22 @@ type Story = StoryObj<typeof Sidebar>
 
 export const Light: Story = {
   decorators: [
-    ThemeDecorator(Theme.LIGHT)
+    ThemeDecorator(Theme.LIGHT),
+    (Story) => (
+      <BrowserRouter>
+        <Story />
+      </BrowserRouter>
+    )
   ]
 }
 
 export const Dark: Story = {
   decorators: [
-    ThemeDecorator(Theme.DARK)
+    ThemeDecorator(Theme.DARK),
+    (Story) => (
+      <BrowserRouter>
+        <Story />
+      </BrowserRouter>
+    )
   ]
 }
