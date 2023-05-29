f0c3c88277f033189bd66f50bc9e7f2ae10002a9
Асия Муксимова <asya@MacBook-Pro-Asia.local>
2023-05-29 15:34:19 +0300

    Add tests

---
+++ src/features/AuthByUsername/ui/LoginForm/Login.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { LoginForm } from './LoginForm';
+
+const meta: Meta<typeof LoginForm> = {
+  title: 'feature/LoginForm',
+  component: LoginForm,
+  tags: ['autodocs'],
+  argTypes: {
+    //backgroundColor: { control: 'color' },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof LoginForm>;
+
+export const Primary: Story = {
+  args: {
+    
+  }
+}
+
---
+++ src/shared/ui/Input/Input.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Input } from './Input';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+
+const meta: Meta<typeof Input> = {
+  title: 'shared/Input',
+  component: Input,
+  tags: ['autodocs'],
+  argTypes: {
+    //backgroundColor: { control: 'color' },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Input>;
+
+export const Primary: Story = {
+  args: {
+    placeholder: 'Input text',
+    value: '1232132'
+  }
+}
+
